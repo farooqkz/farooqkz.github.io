95ba49425b7b9a65994fa85ede312256eb6ccf55
farooqkz <farooqkz@users.noreply.github.com>
2024-10-09 10:41:08 +0000

    deploy: 541a902dbe56dcfd820c65b13ad24938c3b1d1b4

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,12 +1,12 @@
-const toggleSwitch = document.querySelector('i.dark-mode.icons.fas');
+const toggleSwitch = document.querySelector('#dark-mode');
 var currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
     if (currentTheme === 'dark') {
-        document.querySelector('i.dark-mode.icons.fas').classList.remove('fa-moon');
-        document.querySelector('i.dark-mode.icons.fas').classList.add('fa-sun');
+        document.querySelector('#dark-mode .icons.fas').classList.remove('fa-moon');
+        document.querySelector('#dark-mode .icons.fas').classList.add('fa-sun');
     }
 } else {
     currentTheme = localStorage.setItem('theme', 'light')
@@ -18,15 +18,23 @@ function switchTheme() {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
         currentTheme = localStorage.getItem('theme');
-        document.querySelector('i.dark-mode.icons.fas').classList.remove('fa-moon');
-        document.querySelector('i.dark-mode.icons.fas').classList.add('fa-sun');
+        document.querySelector('#dark-mode .icons.fas').classList.remove('fa-moon');
+        document.querySelector('#dark-mode .icons.fas').classList.add('fa-sun');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
         currentTheme = localStorage.getItem('theme');
-        document.querySelector('i.dark-mode.icons.fas').classList.remove('fa-sun');
-        document.querySelector('i.dark-mode.icons.fas').classList.add('fa-moon');
+        document.querySelector('#dark-mode .icons.fas').classList.remove('fa-sun');
+        document.querySelector('#dark-mode .icons.fas').classList.add('fa-moon');
     }
 }
 
 toggleSwitch.addEventListener('click', switchTheme, false);
+
+window.onload = function() {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches && !localStorage.getItem('prefers-color-scheme-dark')) {
+        localStorage.setItem('theme', 'light');
+        switchTheme();
+        localStorage.setItem('prefers-color-scheme-dark', true);
+    }
+};
